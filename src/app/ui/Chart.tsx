55bee03574fef228318bbf4df9468621abdda02a
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-08 15:52:51 -0800

    feat: add chart component

---
+++ src/app/ui/Chart.tsx
@@ -0,0 +1,29 @@
+"use client";
+import * as React from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
+import Paper from "@mui/material/Paper";
+
+export default function Chart({ title }: { title: string }) {
+  return (
+    <>
+      <h1 className="p-2 text-lg semi-bold">{title}</h1>
+
+      <Paper className="bg-softBg rounded-md">
+        <BarChart
+          sx={{
+            ".MuiChartsAxis-root .MuiChartsAxis-tickLabel": { fill: "white" },
+          }}
+          xAxis={[
+            { scaleType: "band", data: ["group A", "group B", "group C"] },
+          ]}
+          series={[
+            { data: [4, 3, 5] },
+            { data: [1, 6, 3] },
+            { data: [2, 5, 6] },
+          ]}
+          height={400}
+        />
+      </Paper>
+    </>
+  );
+}
