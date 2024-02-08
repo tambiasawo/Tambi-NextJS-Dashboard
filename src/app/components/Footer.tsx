d5e2200f2a3587ecd95f2874daf00483483148de
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-08 15:52:41 -0800

    fix(style)

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 ">
+    <footer className="bg-gray-800 text-white p-4 mt-3">
       <p>&copy; 2024 Your Company. All rights reserved.</p>
     </footer>
   );
