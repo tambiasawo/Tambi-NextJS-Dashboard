54d9b90fb18403ff77d6d36ec89408fe37f6e139
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-08 09:09:16 -0800

    feat: add card to dashboard

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@/app/ui/Card";
+import AnnouncementIcon from "@mui/icons-material/Announcement";
 
 const Dashboard = () => {
-  return <div>dahsboard</div>;
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card
+            icon={<AnnouncementIcon />}
+            heading="Total Users"
+            main={10.23}
+            footer="12% more than previous year"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <Card
+            icon={<AnnouncementIcon />}
+            heading="Total Users"
+            main={10.23}
+            footer="12% more than previous year"
+          />
+        </Grid>
+        <Grid item xs={12} sm={12} md={4}>
+          <Card
+            icon={<AnnouncementIcon />}
+            heading="Total Users"
+            main={10.23}
+            footer="-10% more than previous year"
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/app/ui/Card.tsx
@@ -0,0 +1,35 @@
+import { Icon, Stack } from "@mui/material";
+import React from "react";
+
+type Props = {
+  icon: React.ReactElement;
+  heading: string;
+  main: number;
+  footer: string;
+};
+
+export const convertStringToFloat = (string: string) => parseFloat(string);
+
+const Card = ({ icon, heading, main, footer }: Props) => {
+  let percentageValue = convertStringToFloat(footer);
+
+  return (
+    <div className="rounded-xl bg-softBg py-3 px-3 flex justify-start gap-10 items-baseline h-[150px] ">
+      <Icon>{icon} </Icon>
+      <Stack className="flex flex-col" spacing={2}>
+        <h2 className="text-xl">{heading}</h2>
+        <p className="text-2xl">{main}</p>
+        <p className="text-sm">
+          <span
+            className={percentageValue < 0 ? "text-red-500" : "text-green-500"}
+          >
+            {percentageValue + "% "}
+          </span>
+          {footer.substring(footer.indexOf("more"))}
+        </p>
+      </Stack>
+    </div>
+  );
+};
+
+export default Card;
