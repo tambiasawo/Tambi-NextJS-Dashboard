b61dfa8096f99e404b157ec27bc46757add061c2
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-08 15:52:28 -0800

    feat: add schema  and model

---
+++ src/app/db/schema.js
@@ -0,0 +1,38 @@
+import { Schema, models, model } from "mongoose";
+
+const ProductSchema = new Schema({
+  title: {
+    required: true,
+    type: String,
+    unique: true,
+    min: 2,
+    max: 100,
+  },
+  description: {
+    required: false,
+    type: String,
+  },
+  price: {
+    required: true,
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+  },
+  stock: {
+    type: Number,
+  },
+  size: {
+    type: Number,
+  },
+  color: {
+    required: true,
+    type: String,
+  },
+  img: {
+    required: true,
+    type: String,
+  },
+});
+
+export const Product = models.Product || model("Product", ProductSchema);
