9242e3fc016a1951477e068c5a7f7d8be39a8a49
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-09 11:23:42 -0800

    feat: implement dialog form

--- src/app/ui/Toolbar.tsx
+++ src/app/ui/Toolbar.tsx
@@ -1,13 +1,24 @@
 "use client";
 import React from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Dialog, DialogTitle, DialogProps } from "@mui/material";
+import styles from "@/app/styles/FormDialog.module.css";
+import { addProduct } from "../actions";
 
 const Toolbar = () => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
   const params = new URLSearchParams(searchParams);
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value !== "") {
       params.set("q", e.target.value);
@@ -30,8 +41,54 @@ const Toolbar = () => {
         />
       </div>
       <div>
-        <button className="bg-[#5d57c9] px-3 py-2 rounded-lg">Add New</button>
+        <button
+          className="bg-[#5d57c9] px-3 py-2 rounded-lg"
+          onClick={handleClickOpen}
+        >
+          Add New
+        </button>
       </div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        fullWidth={true}
+        maxWidth={"md"}
+        PaperProps={{
+          //component: "form",
+          style: { backgroundColor: "var(--softBg)" },
+        }}
+      >
+        <DialogTitle className="text-white">Add New Product</DialogTitle>
+        <div>
+          <form
+            className={styles.form}
+            action={addProduct}
+            onSubmit={handleClose}
+          >
+            <div className="flex justify-between gap-4">
+              <input type="text" placeholder="Title" name="title" />
+              <input type="number" placeholder="Price" name="price" />
+            </div>
+            <div className="flex justify-between gap-4">
+              <input type="text" placeholder="Color" name="color" />
+              <input type="number" placeholder="Stock " name="stock" />
+            </div>
+
+            <div className="flex items-baseline gap-4">
+              <input type="url" name="img" placeholder="Photo URL" />
+              <textarea
+                name="description"
+                rows={5}
+                className=""
+                placeholder="Description"
+              />
+            </div>
+            <button className="bg-[#5d57c9] py-3 rounded" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </Dialog>
     </div>
   );
 };
