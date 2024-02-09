372442c1728eee956c74dbac299ff88bacc9a6ea
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-09 11:22:12 -0800

    feat: add product schema

--- src/app/db/schema.js
+++ src/app/db/schema.js
@@ -1,38 +1,41 @@
 import { Schema, models, model } from "mongoose";
 
-const ProductSchema = new Schema({
-  title: {
-    required: true,
-    type: String,
-    unique: true,
-    min: 2,
-    max: 100,
+const ProductSchema = new Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+      unique: true,
+      min: 2,
+      max: 100,
+    },
+    description: {
+      required: false,
+      type: String,
+    },
+    price: {
+      required: true,
+      type: Number,
+    },
+    createdAt: {
+      type: Date,
+    },
+    stock: {
+      type: Number,
+    },
+    size: {
+      type: Number,
+    },
+    color: {
+      required: true,
+      type: String,
+    },
+    img: {
+      required: true,
+      type: String,
+    },
   },
-  description: {
-    required: false,
-    type: String,
-  },
-  price: {
-    required: true,
-    type: Number,
-  },
-  createdAt: {
-    type: Date,
-  },
-  stock: {
-    type: Number,
-  },
-  size: {
-    type: Number,
-  },
-  color: {
-    required: true,
-    type: String,
-  },
-  img: {
-    required: true,
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
-export const Product = models.Product || model("Product", ProductSchema);
+export const Product = models?.Product || model("Product", ProductSchema);
