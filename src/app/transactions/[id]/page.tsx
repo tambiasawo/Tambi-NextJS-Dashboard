06e0df66d2e32020a0ecc347731f8f30d0b3d3ab
Tambi <tambi.asawo@verticalmotion.ca>
2024-02-14 15:07:44 -0800

    fix: add required to html fields

--- src/app/transactions/[id]/page.tsx
+++ src/app/transactions/[id]/page.tsx
@@ -12,11 +12,26 @@ const SingleTransaction = async ({ params }: { params: { id: string } }) => {
         <form className={styles.form} action={updateTransaction}>
           <label className="text-[22px]">Recipient</label>
           <input type="text" name="id" hidden value={trans.id} readOnly />
-          <input type="text" name="recipient" placeholder={trans.recipient} />
+          <input
+            type="text"
+            name="recipient"
+            placeholder={trans.recipient}
+            required
+          />
           <label>Invoice Number</label>
-          <input type="number" name="invoice" placeholder={trans.invoice} />
+          <input
+            type="number"
+            name="invoice"
+            placeholder={trans.invoice}
+            required
+          />
           <label>Amount</label>
-          <input type="number" name="amount" placeholder={trans.amount} />
+          <input
+            type="number"
+            name="amount"
+            placeholder={trans.amount}
+            required
+          />
 
           <label>Description</label>
           <textarea
